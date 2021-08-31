aeb4b9f068bedbf6da561d2b497bff97ee00c639
149203 <149203@gmail.com>
2021-08-31 15:36:26 -0400

    add bootstrap

---
+++ gatsby-browser.js
@@ -0,0 +1,12 @@
+// import preact devtools https://preactjs.github.io/preact-devtools/
+// import "preact/debug";
+
+// import global style
+import "./src/style/index.scss";
+
+// We will need to install Popper if using Bootstrap tooltips: npm i @popperjs/core
+// import "@popperjs/core";
+
+// import only the Bootstrap js needed
+// https://getbootstrap.com/docs/5.0/customize/optimize/
+import "bootstrap/js/dist/collapse";
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.howtonameimages.com",
-    title: "How To Name Images",
-  },
-  plugins: [],
+   siteMetadata: {
+      siteUrl: "https://www.howtonameimages.com",
+      title: "How To Name Images",
+   },
+   plugins: [
+      {
+         resolve: `gatsby-plugin-sass`,
+         options: {
+            implementation: require("sass"),
+            postCssPlugins: [],
+            sassOptions: {
+               precision: 10,
+            },
+         },
+      },
+   ],
 };
