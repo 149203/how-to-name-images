c90b3f557957dfc1460b825315081bc798d753f9
149203 <149203@gmail.com>
2021-08-31 15:31:41 -0400

    scrape out gatsby starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.howtonameimages.com",
     title: "How To Name Images",
   },
   plugins: [],
